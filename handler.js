5c67d792f9cc36e56cc9e3dee8c149adfd720b19
fquevedo <fdo.quevedo.alonso@gmail.com>
2017-02-02 21:43:22 -0300

    Added readme.me

--- handler.js
+++ handler.js
@@ -1,13 +1,15 @@
 'use strict';
 
 var http = require('http');
+// import request&scraping module
 var request_scraping = require("./request&scraping.js");
 
 module.exports.getTrackingInfo = (event, context, callback) => {
-
+//Get id parameter (tracking number) through url using event object
   var trcking_number = event.path.id;
-
+//calls tracking function of request_scraping module
   request_scraping.tracking(trcking_number).then(function(r){
+
       callback(null, r);
     });
   
