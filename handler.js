9fda55f158498a285beed5f3a46ccf83c525086f
Fernando Quevedo <fdo.quevedo.alonso@gmail.com>
2017-02-03 14:33:11 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,9 +4,9 @@ var http = require('http');
 var request_scraping = require("./request&scraping.js");
 
 module.exports.getTrackingInfo = (event, context, callback) => {
-
+  //get tracking number (id) from event object
   var trcking_number = event.path.id;
-
+  //call tracking function 
   request_scraping.tracking(trcking_number).then(function(r){
       callback(null, r);
     });
